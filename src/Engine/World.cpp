68fa377a21122508fb8808bed778a54f260e6cff
Vivien Singier <vsingier@gaming.tech>
2026-03-10 18:00:03 +0100

    [REF]

--- src/Engine/World.cpp
+++ src/Engine/World.cpp
@@ -1,1 +1,20 @@
-PCH#include "World.h"
+#include "World.h"
+
+void World::MoveEntity(EntityId _entity, EntityRecord& _rec, Archetype* _src, Archetype* _dst)
+{
+    uint32 srcRow = _rec.row;
+
+    for (auto& [cid, col] : _src->storage.columns)
+    {
+        if (_dst->mask.test(cid))
+        {
+            uint64 stride = _src->storage.strides[cid];
+            Byte* raw = _src->storage.GetRaw(cid, srcRow);
+
+            Vector<Byte>& dstCol = _dst->storage.columns[cid];
+            dstCol.insert(dstCol.end(), raw, raw + stride);
+        }
+    }
+
+    uint32 dstRow =
+}
