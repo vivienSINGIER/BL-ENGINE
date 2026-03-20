303276af7ae346338dc88de66d08bcdc21edceb4
Vivien Singier <vsingier@gaming.tech>
2026-03-20 09:31:58 +0100

    [REF] network base

--- src/Engine/Network/Client.h
+++ src/Engine/Network/Client.h
@@ -4,6 +4,7 @@
 #include "INetworkBase.h"
 class Client : public INetworkBase
 {
+    void
 };
 
 #endif // !CLIENT_H_DEFINED
--- src/Engine/Network/INetworkBase.h
+++ src/Engine/Network/INetworkBase.h
@@ -21,7 +21,7 @@ class INetworkBase
 public:
 	INetworkBase();
 
-	virtual void Update() = 0;
+	virtual void Update(float _dt) = 0;
 	virtual void SendPackets() = 0;
 	virtual void RegisterPacket(Packet _packet) = 0;
 	virtual void HandlePacket() = 0;
@@ -29,23 +29,23 @@ public:
 	Sockets* GetSocket() const { return m_socket; }
 
 	virtual ~INetworkBase() = default;
+	
 protected:
-	void SendReliablePacket(Packet _packet, sockaddr_in _target);
-	void TickAck(float _deltaTime);
-	void OnAckReceived(uint16 _ackId, sockaddr_in _sender);
-	void SendAck(uint16 _ackId, sockaddr_in _target);
-
 	const static inline float ACK_DELAY = 1.0f;
 	const static inline uint8 MAX_RETRIES = 10;
 	static inline uint16 ACK_COUNT = 0;
 	
 	Sockets* m_socket;
-	Byte m_buffer[1024] = {};
+	char m_buffer[sizeof(Packet)] = {};
 	Vector<PendingPacket> m_pendingPackets;
 	Vector<Packet> m_packets;
 
 	CriticalSection m_packetProtection;
-
+	
+	void SendReliablePacket(Packet _packet, sockaddr_in _target);
+	void TickAck(float _deltaTime);
+	void OnAckReceived(uint16 _ackId, sockaddr_in _sender);
+	void SendAck(uint16 _ackId, sockaddr_in _target);
 };
 
 #endif // !I_NETWORK_BASE_H_DEFINED
