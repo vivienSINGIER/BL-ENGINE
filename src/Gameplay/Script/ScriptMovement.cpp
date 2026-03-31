c72e938927327c5ee6de166ba5c535e5f82f4f3e
Hexor <avilleret@gaming.tech>
2026-04-01 00:19:31 +0200

    [FIX]

--- src/Gameplay/Script/ScriptMovement.cpp
+++ src/Gameplay/Script/ScriptMovement.cpp
@@ -18,7 +18,7 @@ void Movement::ScriptMovement::Update(float dt)
 	m_yaw += mouseDelta.x * mouseSensitivity;
 	m_pitch += mouseDelta.y * mouseSensitivity;
 
-	m_pitch = Clamp(m_pitch, -XM_PIDIV2 + 0.01f, XM_PIDIV2 - 0.01f);
+	m_pitch = Clamp(m_pitch, -59.0f * (XM_PI / 180.0f), 59.0f * (XM_PI / 180.0f));
 
 	if (m_cursorLocked)
 	{
