174b4ec5d313e3e33af423e11e2bd0833f38251e
Vivien Singier <vsingier@gaming.tech>
2026-04-03 13:00:08 +0200

    [ADD] Start

--- src/Render/Generic/Factories/SpriteFactory.hpp
+++ src/Render/Generic/Factories/SpriteFactory.hpp
@@ -34,6 +34,34 @@ public:
 
         return sprite;
     }
+
+    static Sprite* BuildRoundedRectangle(Device* _pDevice, int _width, int _height, int _radius, bool _isDynamic = false)
+    {
+        Sprite* sprite = _pDevice->CreateSprite();
+
+        Vector<UiVertex> vertices;
+        Vector<uint32> indices;
+
+        float midWidth = (float)_width / 2;
+        float midHeight = (float)_height / 2;
+        
+        vertices = {
+            UiVertex(XMFLOAT2(-midWidth, midHeight), XMFLOAT2(0.0f, 0.0f)),
+            UiVertex(XMFLOAT2(midWidth, midHeight), XMFLOAT2(1.0f, 0.0f)),
+            UiVertex(XMFLOAT2(-midWidth, -midHeight), XMFLOAT2(0.0f, 1.0f)),
+            UiVertex(XMFLOAT2(midWidth, -midHeight), XMFLOAT2(1.0f, 1.0f)),
+        };
+
+        indices = {
+            0, 1, 2,
+            2, 1, 3
+        };
+
+        sprite->SetVertexData(vertices.data(), vertices.size());
+        sprite->SetIndexData(indices.data(), indices.size());
+
+        return sprite;
+    }
 };
 
 #endif
